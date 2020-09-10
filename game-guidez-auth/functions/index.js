7326daf25b5f81d20cc75ad2b52d38cbfcc26dc0
Traian <niculescu.traian.ct@gmail.com>
2020-09-10 10:17:18 +0300

    cloud functions before branching

--- game-guidez-auth/functions/index.js
+++ game-guidez-auth/functions/index.js
@@ -6,4 +6,39 @@ admin.initializeApp();
 
 // create function that will make a specific user an admin
 // onCall - we can call this function from the front-end
-exports.addAdminRole = functions.https.onCall;
+
+// data is gonna include any custom data that we send along with this call to the function
+// context - contains information about the authentication of the user that made this call to the function
+
+// get the user and add a custom claim to the user (admin)
+// exports.addAdminRole = functions.https.onCall((data, context) => {
+//   return admin
+//     .auth()
+//     .getUserByEmail(data.email)
+//     .then((user) => {
+//       return admin.auth().setCustomUserClaims(user.uid, {
+//         admin: true
+//       });
+//     })
+//     .then(() => {
+//       return {
+//         message: `Success! ${data.email} has been made an admin`
+//       };
+//     })
+//     .catch((err) => console.log(err.message));
+// });
+
+exports.addAdminRole = functions.https.onCall(async (data, context) => {
+  // get the user and add a custom claim to the user (admin)
+  try {
+    const user = await admin.auth().getUserByEmail(data.email);
+    await admin.auth().setCustomUserClaims(user.uid, {
+      admin: true
+    });
+    return {
+      message: `Success! ${data.email} has been made an admin`
+    };
+  } catch (err) {
+    return console.log(err.message);
+  }
+});
